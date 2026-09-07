292fbcda33b110e566159959428e05ede06d2952
Mirage <gijs.van.schaik@outlook.com>
2026-09-07 19:27:23 +0200

    Load runtime secrets before payment routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
+
 const mollieRoutes = require('./routes/mollie');
 const pulsepayRoutes = require('./routes/pulsepay');
 const openBankingRoutes = require('./routes/openbanking');
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
